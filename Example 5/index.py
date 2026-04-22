ce13a1ad6fc21598cc7388105f0d7ee1754b7e5a
cumar-dev <your_email@example.com>
2026-04-22 11:03:32 +0300

    added example 5 python

---
+++ Example 5/index.py
@@ -0,0 +1,60 @@
+a = 10;
+b = 5;
+# # arthematic opeartor
+print("a + b =", a + b);
+print("a - b = ", a - b);
+print("a * b = ", a * b);
+print("a / b =", a / b);
+print("a % b =", a % b);
+print("a // b =", a // b);
+print("a ** b =", a ** b);
+
+# Assignment operator
+
+num1 = 10;
+num2 = 20;
+
+num2 += 10;
+print (num2);
+
+
+num2 //= num1;
+
+print(num2);
+
+num1 += num2;
+print(num1);
+
+# comparison operator
+
+number1 = 10;
+number2 = 10;
+
+print(number1 == number2);
+
+print(number1 != number2);
+
+print(number2 < number1);
+
+print(number1 > number2);
+
+print(number2 <= number1);
+
+print(number1 >= number2);
+
+# logical operator
+
+x = 10;
+y= 6;
+
+print(x == y or x > y);
+
+print(x != y and x < y);
+
+sum = x == y or x > y;
+print(not sum);
+
+score = 85;
+
+print(score < 60 or score > 100);
+
